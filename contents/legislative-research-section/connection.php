c80835e7cb11831f8caf033cbfbb016281212f71
jolo20 <dxjolo@gmail.com>
2025-09-12 13:00:41 +0800

    modal

--- contents/legislative-research-section/connection.php
+++ contents/legislative-research-section/connection.php
@@ -1,16 +1,18 @@
 <?php
+// connections.php
 
-// Database connection details
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "lgu2";
+// Database connection details for lgu2
+$servername_lgu2 = "localhost";
+$username_lgu2 = "root";
+$password_lgu2 = "";
+$dbname_lgu2 = "lgu2";
 
-// Create a connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+// Create connection to lgu2 database
+$lgu2_conn = new mysqli($servername_lgu2, $username_lgu2, $password_lgu2, $dbname_lgu2);
 
-// Check connection
-if ($conn->connect_error) {
-    echo json_encode(['status' => 'error', 'message' => 'Connection failed: ' . $conn->connect_error]);
-    exit();
+// Check lgu2 connection
+if ($lgu2_conn->connect_error) {
+    die("Connection to lgu2 failed: " . $lgu2_conn->connect_error);
 }
+
+?>
